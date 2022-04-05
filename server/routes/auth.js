deed094c0a00965769a712107d8224fc95a60fde
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-04-05 23:15:35 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const { sendOtp, verifyOtp, login } = require('../controllers/authController');
 
-// const { userRegisterValidator , userVerifyValidator} = require('../validators/auth');
-// const { runValidation } = require('../validators');
 
 // Routes
 router.post('/send-otp',  sendOtp);
