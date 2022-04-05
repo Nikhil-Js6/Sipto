4aef6be0d106fa988f95c8be46160eb2d65e1b1d
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-04-05 23:36:46 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { sendOtp, verifyOtp, login } = require('../controllers/authController');
+const { sendRegisterOtp, verifyRegisterOtp, sendLoginOtp, verifyLoginOtp } = require('../controllers/authController');
 
 
 // Routes
-router.post('/send-otp',  sendOtp);
-router.post('/verify-otp', verifyOtp);
-router.post('/login', login);
+router.post('/register/send-otp', sendRegisterOtp);
+router.post('/register/verify-otp', verifyRegisterOtp);
+router.post('/login/send-otp',  sendLoginOtp);
+router.post('/login/verify-otp', verifyLoginOtp);
 
 module.exports = router;
